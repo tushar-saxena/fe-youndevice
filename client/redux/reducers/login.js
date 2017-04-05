40dea9e6d1e07e8b5478c40440bb79f7cda5e070
Khushboo Chanana <khushboo.chanana@tothenew.com>
2017-04-05 16:25:57 +0530

    Add hit via fetch api

--- client/redux/reducers/login.js
+++ client/redux/reducers/login.js
@@ -4,12 +4,28 @@ export default function login(state = false, action) {
 	console.log(">>action" ,action);
 	switch (action.type) {
 		case 'LOGIN':
-			var loginAPI = "http://api.youndevice.com/api/v1/login";
-			$.post(loginAPI,{ "emailId": "tushar.saxena@tothenew.com",
-				"password": "tushar"}).done((data) =>{
-				console.log(data);
-			     return  true;
-			});
+			fetch('http://10.1.22.109/api/v1/login', {
+				method: 'POST',
+								mode: 'cors',
+
+				headers: {
+					'Access-Control-Allow-Origin':'*',
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body:JSON.stringify( {emailId: "tushar.saxena@tothenew.com",
+					password: "tushar"})
+
+			})
+					.then(function(response) {
+						return true
+					}).then(function(body) {
+						console.log(body);
+
+					});
+			return true;
+
+
 		default:
 			return state;
 	}
