b9a339fd5d51c7ef690c53bc6b4b2684657fc7d2
Khushboo Chanana <khushboo.chanana@tothenew.com>
2017-04-12 15:05:19 +0530

    Add logout functionality

--- client/components/home.js
+++ client/components/home.js
@@ -1,16 +1,24 @@
 import React, { PropTypes,Component } from 'react'
 import {Jumbotron} from 'react-bootstrap';
 import {browserHistory,Link } from 'react-router'
+import cookie from 'react-cookie'
 
 export default class Home extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
+		this.logout = this.logout.bind(this);
+
 	}
+	logout=()=>{
+		cookie.remove('loginToken', { path: '/' });
+		browserHistory.push('/')
+
+	};
 	render() {
 		console.log(this.props);
 		return (
 				<div className="overview-page" key="overview">
-						<a className="pull-right btn btn-primary btn-outline btn-rounded"> Logout</a>
+						<a className="pull-right btn btn-primary btn-outline btn-rounded" onClick={this.logout}> Logout</a>
 					<h2>Home <small></small></h2>
 					<Jumbotron>
 						<h1>Welcome!</h1>!
