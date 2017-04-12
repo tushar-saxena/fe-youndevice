614d8e6375f0a9cbff9bdca59b633eddd451527b
Khushboo Chanana <khushboo.chanana@tothenew.com>
2017-04-12 15:00:41 +0530

    Modify file name

--- client/containers/register.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-import { register } from '../redux/actions/index'
-import RegisterForm from '../components/regiter'
-
-export default connect(
-		{
-			register: (username, password, email)=> register(username, password, email)
-		}
-)(RegisterForm)
