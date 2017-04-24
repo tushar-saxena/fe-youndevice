a3f94ebea5ec0bd0963102d86b839a7e699e3a09
Khushboo Chanana <khushboo.chanana@tothenew.com>
2017-04-24 09:37:05 +0530

    Redirect to login if no token found in routes

--- client/index.js
+++ client/index.js
@@ -4,11 +4,23 @@ import App from './components/App'
 import Home from './components/home'
 import store from './redux/store/config'
 import React from 'react'
+import cookie from 'react-cookie';
+
 import "./common/styles/app.less";
 import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
+
+function redirectToLogin(store) {
+    return (nextState, replace) => {
+        if (!cookie.load('loginToken')) {
+            replace({
+                pathname: '/login'
+            });
+        }
+    };
+}
 
 
 render(
@@ -16,9 +28,8 @@ render(
             <Router history={history}>
                 <Route path="/" component={App}>
                     <IndexRoute component={Main}/>
-
                     <Route path="/login" component={Main}/>
-                    <Route path="/home" component={Home}/>
+                    <Route path="/home" onEnter={redirectToLogin(store)} component={Home}/>
                 </Route>
             </Router>
         </Provider>,
